8e61e6f2401bcca1a5afe5db4e6b8ef6db4e8b3e
ejazuddin55 <71565879+ejazuddin55@users.noreply.github.com>
2020-09-20 23:02:46 +0500

    Create Random forest .py

---
+++ Random forest .py
@@ -0,0 +1,101 @@
+import librosa
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import csv
+# Preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+# Keras
+import keras
+from keras import models
+from keras import layers
+from  sklearn import svm
+import sklearn
+from sklearn import linear_model
+
+
+header = 'filename chroma_stft rmse spectral_centroid spectral_bandwidth rolloff zero_crossing_rate'
+for i in range(1, 21):
+    header += f' mfcc{i}'
+header += ' label'
+header = header.split()
+
+file = open('data.csv', 'w', newline='')
+with file:
+    writer = csv.writer(file)
+    writer.writerow(header)
+songs = 'minge pak'.split()
+for g in songs:
+    for filename in os.listdir(f'./songs/{g}'):
+        songname = f'./songs/{g}/{filename}'
+        y, sr = librosa.load(songname, mono=True, duration=10 )
+        chroma_stft = librosa.feature.chroma_stft(y=y, sr=sr)
+        rmse = librosa.feature.rmse(y=y)
+        spec_cent = librosa.feature.spectral_centroid(y=y, sr=sr)
+        spec_bw = librosa.feature.spectral_bandwidth(y=y, sr=sr)
+        rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr)
+        zcr = librosa.feature.zero_crossing_rate(y)
+        mfcc = librosa.feature.mfcc(y=y, sr=sr)
+        to_append = f'{filename} {np.mean(chroma_stft)} {np.mean(rmse)} {np.mean(spec_cent)} {np.mean(spec_bw)} {np.mean(rolloff)} {np.mean(zcr)}'
+        for e in mfcc:
+            to_append += f' {np.mean(e)}'
+        to_append += f' {g}'
+        file = open('data.csv', 'a', newline='')
+        with file:
+            writer = csv.writer(file)
+            writer.writerow(to_append.split())
+
+# reading dataset from csv
+
+data = pd.read_csv('data.csv', error_bad_lines=False, quoting=csv.QUOTE_NONE, warn_bad_lines=False)
+
+# data.head()
+print(data)
+# data.to_excel('test.xlsx') for csv files as a excel file
+
+# Dropping unneccesary columns
+data = data.drop(['filename'], axis=1)
+data.head()
+
+genre_list = data.iloc[:, -1]
+encoder = LabelEncoder()
+y = encoder.fit_transform(genre_list)
+print(y)
+
+
+# normalizing
+scaler = StandardScaler()
+X = scaler.fit_transform(np.array(data.iloc[:, :-1], dtype=float))
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3) # 70% training and 30% test
+from sklearn.ensemble import RandomForestClassifier
+
+#Create a Gaussian Classifier
+clf=RandomForestClassifier(n_estimators=100)
+
+#Train the model using the training sets y_pred=clf.predict(X_test)
+clf.fit(X_train,y_train)
+
+y_pred=clf.predict(X_test)
+from sklearn import metrics
+print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+import matplotlib.pyplot as plt
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import plot_roc_curve
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+svc = SVC(kernel= "linear" , C = 2 ,random_state=42)
+svc.fit(X_train, y_train)
+svc_disp = plot_roc_curve(svc, X_test, y_test)
+plt.show()
+
+rfc = RandomForestClassifier(n_estimators=10, random_state=42)
+rfc.fit(X_train, y_train)
+ax = plt.gca()
+rfc_disp = plot_roc_curve(rfc, X_test, y_test, ax=ax, alpha=0.8)
+svc_disp.plot(ax=ax, alpha=0.8)
+plt.show()
+
